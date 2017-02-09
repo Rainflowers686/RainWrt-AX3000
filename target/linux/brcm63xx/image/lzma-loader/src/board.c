Rainflowers686/RainWrt-AX3000

commit 1865df97943d2f962aa8a038f75662ab2fa55b1e

brcm63xx: lzma-loader: add BCM3380 support
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
Signed-off-by: Jonas Gorski <jogo@openwrt.org>

SVN-Revision: 47377

target/linux/brcm63xx/image/lzma-loader/src/board.c
@@ -70,6 +70,8 @@ void board_init(void)
 	case PRID_IMP_BMIPS43XX:
 		if ((prid & 0xff) == 0x04)
 			chipid_reg = 0xfff8c000;
+		else if ((prid & 0xff) == 0x70)
+			chipid_reg = 0xb4e00000;
 		else if ((prid & 0xff) >= 0x30)
 			chipid_reg = 0xb0000000;
 		else
@@ -94,6 +96,9 @@ void board_init(void)
 	case 0x6316:
 	case 0x6326:
 		uart_base = chipid_reg + 0x180;
+		break;
+	case 0x3380:
+		uart_base = chipid_reg + 0x200;
 		break;
 	case 0x6338:
 	case 0x6345:

commit 963286b4c07bee3d5c992c5f29f9402f6e3bb9c9

brcm63xx: fix lzma loader for BCM6362
BCM6362 and BCM3380 seem to share the same PRID. Disable serial output
for them until we can find a way to tell them apart reliably.

Fixes: 1865df9794 ("brcm63xx: lzma-loader: add BCM3380 support")
Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

target/linux/brcm63xx/image/lzma-loader/src/board.c
@@ -71,7 +71,7 @@ void board_init(void)
 		if ((prid & 0xff) == 0x04)
 			chipid_reg = 0xfff8c000;
 		else if ((prid & 0xff) == 0x70)
-			chipid_reg = 0xb4e00000;
+			return;	/* FIXME: 0002a070 can be 6362 and 3380 */
 		else if ((prid & 0xff) >= 0x30)
 			chipid_reg = 0xb0000000;
 		else