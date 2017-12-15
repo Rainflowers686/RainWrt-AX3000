Rainflowers686/RainWrt-AX3000

commit 3c57d23cfcbeeb7fda598f4d61fd6f30939322bb

ar71xx: Add kernel 4.9 support
This add support for kernel 4.9 to the ar71xx target.
It was compile tested with the generic, NAND and mikrotik subtarget.
Multiple members of the community tested it on their boards and did not
report any major problem so far.

Especially the NAND part received some changes to adapt to the new
kernel APIs. The serial driver hack used for the Arduino Yun was not
ported because the kernel changed there a lot.

Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/ar71xx/files/arch/mips/ath79/mach-c60.c
@@ -22,6 +22,7 @@
 #include <linux/platform/ar934x_nfc.h>
 #include <linux/ar8216_platform.h>
 #include <linux/ath9k_platform.h>
+#include <linux/version.h>
 
 #include <asm/mach-ath79/ar71xx_regs.h>
 
@@ -119,6 +120,7 @@ static struct mdio_board_info c60_mdio0_info[] = {
 	},
 };
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,6,0)
 static struct nand_ecclayout c60_nand_ecclayout = {
 	.eccbytes       = 7,
 	.eccpos         = { 4, 8, 9, 10, 13, 14, 15 },
@@ -126,13 +128,67 @@ static struct nand_ecclayout c60_nand_ecclayout = {
 	.oobfree        = { { 0, 3 }, { 6, 2 }, { 11, 2 }, }
 };
 
+#else
+
+static int c60_ooblayout_ecc(struct mtd_info *mtd, int section,
+			     struct mtd_oob_region *oobregion)
+{
+	switch (section) {
+	case 0:
+		oobregion->offset = 4;
+		oobregion->length = 1;
+		return 0;
+	case 1:
+		oobregion->offset = 8;
+		oobregion->length = 3;
+		return 0;
+	case 2:
+		oobregion->offset = 13;
+		oobregion->length = 3;
+		return 0;
+	default:
+		return -ERANGE;
+	}
+}
+
+static int c60_ooblayout_free(struct mtd_info *mtd, int section,
+			      struct mtd_oob_region *oobregion)
+{
+	switch (section) {
+	case 0:
+		oobregion->offset = 0;
+		oobregion->length = 3;
+		return 0;
+	case 1:
+		oobregion->offset = 6;
+		oobregion->length = 2;
+		return 0;
+	case 2:
+		oobregion->offset = 11;
+		oobregion->length = 2;
+		return 0;
+	default:
+		return -ERANGE;
+	}
+}
+
+static const struct mtd_ooblayout_ops c60_nand_ecclayout_ops = {
+	.ecc = c60_ooblayout_ecc,
+	.free = c60_ooblayout_free,
+};
+#endif /* < 4.6 */
+
 static int c60_nand_scan_fixup(struct mtd_info *mtd)
 {
 	struct nand_chip *chip = mtd->priv;
 
 	chip->ecc.size = 512;
 	chip->ecc.strength = 4;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,6,0)
 	chip->ecc.layout = &c60_nand_ecclayout;
+#else
+	mtd_set_ooblayout(mtd, &c60_nand_ecclayout_ops);
+#endif
 	return 0;
 }
 

commit 07a7b0c8443524a4a73be54472de1ee92aa1e335

ar71xx: fix invalid pointer dereference in c60_nand_scan_fixup()
Since Linux 4.6, mtd->priv no longer points to the NAND specific
structure. Under 4.9 it contains NULL, thus using it to access
the fields of the nand_chip structure causes an invalid pointer
dereference.

Update the code to use the mtd_to_nand() helper under 4.9 to obtain
the address of the chip specific data.

Compile tested only.

Fixes: 3c57d23cfcbe ("ar71xx: Add kernel 4.9 support")
Signed-off-by: Gabor Juhos <juhosg@freemail.hu>
Tested-by: Christian Lamparter <chunkeey@gmail.com>

target/linux/ar71xx/files/arch/mips/ath79/mach-c60.c
@@ -180,7 +180,11 @@ static const struct mtd_ooblayout_ops c60_nand_ecclayout_ops = {
 
 static int c60_nand_scan_fixup(struct mtd_info *mtd)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,6,0)
 	struct nand_chip *chip = mtd->priv;
+#else
+	struct nand_chip *chip = mtd_to_nand(mtd);
+#endif
 
 	chip->ecc.size = 512;
 	chip->ecc.strength = 4;