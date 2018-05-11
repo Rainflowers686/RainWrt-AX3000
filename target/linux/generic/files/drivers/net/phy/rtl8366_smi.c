Rainflowers686/RainWrt-AX3000

commit 5bbf704b9134d0b049d3e018976b947b5a6329db

kernel: add OF support to rtl8355 driver
Signed-off-by: John Crispin <john@phrozen.org>

target/linux/generic/files/drivers/net/phy/rtl8366_smi.c
@@ -20,6 +20,7 @@
 #include <linux/of_gpio.h>
 #include <linux/rtl8366.h>
 #include <linux/version.h>
+#include <linux/of_mdio.h>
 
 #ifdef CONFIG_RTL8366_SMI_DEBUG_FS
 #include <linux/debugfs.h>
@@ -915,6 +916,11 @@ static inline void rtl8366_debugfs_remove(struct rtl8366_smi *smi) {}
 static int rtl8366_smi_mii_init(struct rtl8366_smi *smi)
 {
 	int ret;
+	struct device_node *np = NULL;
+
+#ifdef CONFIG_OF
+	np = of_get_child_by_name(smi->parent->of_node, "mdio-bus");
+#endif
 
 	smi->mii_bus = mdiobus_alloc();
 	if (smi->mii_bus == NULL) {
@@ -939,7 +945,10 @@ static int rtl8366_smi_mii_init(struct rtl8366_smi *smi)
 	}
 #endif
 
-	ret = mdiobus_register(smi->mii_bus);
+	if (np)
+		ret = of_mdiobus_register(smi->mii_bus, np);
+	else
+		ret = mdiobus_register(smi->mii_bus);
 	if (ret)
 		goto err_free;
 

commit 85ba91d85e0dd765f594bf6eed8a0820214e203a

kernel: rtl8366_smi: guard of_mdiobus_register() call
Make the call to of_mdiobus_register() dependant on CONFIG_OF to avoid
pulling in an unwanted dependency on of_mdio.ko.

Fixes: 5bbf704b91 kernel: add OF support to rtl8355 driver
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

target/linux/generic/files/drivers/net/phy/rtl8366_smi.c
@@ -916,9 +916,10 @@ static inline void rtl8366_debugfs_remove(struct rtl8366_smi *smi) {}
 static int rtl8366_smi_mii_init(struct rtl8366_smi *smi)
 {
 	int ret;
-	struct device_node *np = NULL;
 
 #ifdef CONFIG_OF
+	struct device_node *np = NULL;
+
 	np = of_get_child_by_name(smi->parent->of_node, "mdio-bus");
 #endif
 
@@ -945,10 +946,13 @@ static int rtl8366_smi_mii_init(struct rtl8366_smi *smi)
 	}
 #endif
 
+#ifdef CONFIG_OF
 	if (np)
 		ret = of_mdiobus_register(smi->mii_bus, np);
 	else
+#endif
 		ret = mdiobus_register(smi->mii_bus);
+
 	if (ret)
 		goto err_free;
 