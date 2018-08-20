Rainflowers686/RainWrt-AX3000

commit e51d060aa8d4a6e344bbb935ef3aa6707c6a3b2d

rules.mk: replace iremap when using GCC 8
The original -iremap is replaced by -fmacro-prefix-map in GCC 8

Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

rules.mk
@@ -141,6 +141,10 @@ endif
 
 ifeq ($(or $(CONFIG_EXTERNAL_TOOLCHAIN),$(CONFIG_GCC_VERSION_4_8),$(CONFIG_TARGET_uml)),)
   iremap = -iremap$(1):$(2)
+  # just overwrite iremap for GCC 8.1 and higher to keep backward compatibility
+  ifeq ($(CONFIG_GCC_VERSION_8),y)
+    iremap = -fmacro-prefix-map=$(1)=$(2)
+  endif
 endif
 
 PACKAGE_DIR:=$(BIN_DIR)/packages

commit 577e59d04cc208bde62568a9e6846b969a209d19

toolchain/gcc: add config symbol to determine how to apply path remapping
Added boolean symbol for GCC 8 and higher, when we add newer GCC, we don't have
to modify rules.mk to keep things consistant.

Fixes: e51d060 ("rules.mk: replace iremap when using GCC 8")

Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

rules.mk
@@ -140,10 +140,10 @@ else
 endif
 
 ifeq ($(or $(CONFIG_EXTERNAL_TOOLCHAIN),$(CONFIG_GCC_VERSION_4_8),$(CONFIG_TARGET_uml)),)
-  iremap = -iremap$(1):$(2)
-  # just overwrite iremap for GCC 8.1 and higher to keep backward compatibility
-  ifeq ($(CONFIG_GCC_VERSION_8),y)
+  ifeq ($(CONFIG_GCC_USE_EMBEDDED_PATH_REMAP),y)
     iremap = -fmacro-prefix-map=$(1)=$(2)
+  else
+    iremap = -iremap$(1):$(2)
   endif
 endif
 

toolchain/gcc/Config.version
@@ -10,6 +10,11 @@ config GCC_VERSION_8
 	default y if GCC_USE_VERSION_8
 	bool
 
+config GCC_USE_EMBEDDED_PATH_REMAP
+	default y if GCC_VERSION_8
+	default n
+	bool
+
 config GCC_VERSION
 	string
 	default "5.5.0"		if GCC_VERSION_5