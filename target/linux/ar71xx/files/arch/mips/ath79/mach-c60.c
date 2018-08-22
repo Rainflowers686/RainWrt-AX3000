Rainflowers686/RainWrt-AX3000

commit 7753b14190128c684c24d9a0482b1ca8fa1c8a12

ar71xx: add v4.14 support
adds v4.14 patches for testing but leaves v4.9 as default for now.

Signed-off-by: John Crispin <john@phrozen.org>

target/linux/ar71xx/files/arch/mips/ath79/mach-c60.c
@@ -15,7 +15,11 @@
 #include <linux/pci.h>
 #include <linux/phy.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/mtd/physmap.h>
 #include <linux/platform_device.h>
@@ -115,7 +119,7 @@ static struct ar8327_platform_data c60_ar8327_data = {
 static struct mdio_board_info c60_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &c60_ar8327_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-domywifi-dw33d.c
@@ -21,7 +21,11 @@
 #include <linux/platform_device.h>
 #include <linux/ar8216_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/platform/ar934x_nfc.h>
 
 #include <asm/mach-ath79/ar71xx_regs.h>
@@ -131,7 +135,7 @@ static struct ar8327_platform_data dw33d_qca8337_data = {
 static struct mdio_board_info dw33d_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &dw33d_qca8337_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-nbg6716.c
@@ -24,7 +24,11 @@
 #include <linux/ar8216_platform.h>
 #include <linux/gpio.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/platform/ar934x_nfc.h>
 
 #include <asm/mach-ath79/ar71xx_regs.h>
@@ -242,7 +246,7 @@ static struct ar8327_platform_data nbg6716_ar8327_data = {
 static struct mdio_board_info nbg6716_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &nbg6716_ar8327_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-rb2011.c
@@ -17,7 +17,11 @@
 #include <linux/ath9k_platform.h>
 #include <linux/ar8216_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/flash.h>
@@ -156,7 +160,7 @@ static struct ar8327_platform_data rb2011_ar8327_data = {
 static struct mdio_board_info rb2011_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &rb2011_ar8327_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
@@ -15,7 +15,11 @@
 #include <linux/platform_device.h>
 #include <linux/ath9k_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/74x164.h>
@@ -227,7 +231,7 @@ static struct at803x_platform_data rb91x_at803x_data = {
 static struct mdio_board_info rb91x_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &rb91x_at803x_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-rb922.c
@@ -13,7 +13,11 @@
 #include <linux/platform_device.h>
 #include <linux/ath9k_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/flash.h>
@@ -108,7 +112,7 @@ static struct at803x_platform_data rb922gs_at803x_data = {
 static struct mdio_board_info rb922gs_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = RB922_PHY_ADDR,
+		.mdio_addr = RB922_PHY_ADDR,
 		.platform_data = &rb922gs_at803x_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-rb95x.c
@@ -18,7 +18,11 @@
 #include <linux/ath9k_platform.h>
 #include <linux/ar8216_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/flash.h>
@@ -114,7 +118,7 @@ static struct ar8327_platform_data rb95x_ar8327_data = {
 static struct mdio_board_info rb95x_mdio0_info[] = {
 	{
 		.bus_id = "ag71xx-mdio.0",
-		.phy_addr = 0,
+		.mdio_addr = 0,
 		.platform_data = &rb95x_ar8327_data,
 	},
 };

target/linux/ar71xx/files/arch/mips/ath79/mach-rbsxtlite.c
@@ -17,7 +17,11 @@
 #include <linux/platform_device.h>
 #include <linux/ath9k_platform.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 #include <linux/mtd/nand.h>
+#else
+#include <linux/mtd/rawnand.h>
+#endif
 #include <linux/mtd/partitions.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/flash.h>

commit 88adc2e7adb3d9a547e78abf464137cd1b943691

ar71xx: add missing include for checking kernel version
Fixes these build errors:

arch/mips/ath79/mach-rb2011.c:20:5: error: "LINUX_VERSION_CODE" is not defined, evaluates to 0 [-Werror=undef]
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
     ^~~~~~~~~~~~~~~~~~
arch/mips/ath79/mach-rb2011.c:20:26: error: "KERNEL_VERSION" is not defined, evaluates to 0 [-Werror=undef]
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
                          ^~~~~~~~~~~~~~
arch/mips/ath79/mach-rb2011.c:20:40: error: missing binary operator before token "("
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
                                        ^

Fixes: 7753b1419012 ("ar71xx: add v4.14 support")

Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

target/linux/ar71xx/files/arch/mips/ath79/mach-c60.c
@@ -12,6 +12,7 @@
  *  by the Free Software Foundation.
  */
 
+#include <linux/version.h>
 #include <linux/pci.h>
 #include <linux/phy.h>
 #include <linux/mtd/mtd.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-domywifi-dw33d.c
@@ -18,6 +18,7 @@
  *
  */
 
+#include <linux/version.h>
 #include <linux/platform_device.h>
 #include <linux/ar8216_platform.h>
 #include <linux/mtd/mtd.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-nbg6716.c
@@ -20,6 +20,7 @@
  *
  */
 
+#include <linux/version.h>
 #include <linux/platform_device.h>
 #include <linux/ar8216_platform.h>
 #include <linux/gpio.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb2011.c
@@ -11,6 +11,7 @@
 
 #define pr_fmt(fmt) "rb2011: " fmt
 
+#include <linux/version.h>
 #include <linux/phy.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
@@ -10,6 +10,7 @@
 
 #define pr_fmt(fmt) "rb91x: " fmt
 
+#include <linux/version.h>
 #include <linux/phy.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb922.c
@@ -8,6 +8,7 @@
  *  by the Free Software Foundation.
  */
 
+#include <linux/version.h>
 #include <linux/phy.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-rb95x.c
@@ -12,6 +12,7 @@
 
 #define pr_fmt(fmt) "rb95x: " fmt
 
+#include <linux/version.h>
 #include <linux/phy.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-rbsxtlite.c
@@ -12,6 +12,7 @@
 
 #define pr_fmt(fmt) "sxtlite: " fmt
 
+#include <linux/version.h>
 #include <linux/phy.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>

target/linux/ar71xx/files/arch/mips/ath79/mach-wndr4300.c
@@ -9,6 +9,7 @@
  *  by the Free Software Foundation.
  */
 
+#include <linux/version.h>
 #include <linux/pci.h>
 #include <linux/phy.h>
 #include <linux/gpio.h>

target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_ethtool.c
@@ -12,6 +12,7 @@
  */
 
 #include "ag71xx.h"
+#include <linux/version.h>
 
 static int ag71xx_ethtool_get_settings(struct net_device *dev,
 				       struct ethtool_cmd *cmd)