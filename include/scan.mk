Rainflowers686/RainWrt-AX3000

commit e0ffb0f00b0735cd12f2879d3945f351630061c5

build: Honour NO_COLOR in include/scan.mk
The sender domain has a DMARC Reject/Quarantine policy which disallows
sending mailing list messages using the original "From" header.

To mitigate this problem, the original message has been wrapped
automatically by the mailing list software.
Hi all:

This is my first OpenWrt patch. I am a clean, pure newbie! 8-)

Honour NO_COLOR in Makefile function 'progress' in include/scan.mk, in the same way that include/verbose.mk does.

Signed-off-by: R. Diez <rdiezmail-openwrt@yahoo.com>

include/scan.mk
@@ -19,9 +19,15 @@ else
 endif
 
 ifeq ($(IS_TTY),1)
-  define progress
+  ifneq ($(strip $(NO_COLOR)),1)
+    define progress
 	printf "\033[M\r$(1)" >&2;
-  endef
+    endef
+  else
+    define progress
+	:;
+    endef
+  endif
 else
   define progress
 	:;

commit 2ed834a94be0a7383698c502a2fcee8bae90e7a3

build: Fix print without color
The original patch removed the printing completely, just remove the
color.

Fixes: e0ffb0f00b07  ("build: Honour NO_COLOR in include/scan.mk")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

include/scan.mk
@@ -25,7 +25,7 @@ ifeq ($(IS_TTY),1)
     endef
   else
     define progress
-	:;
+	printf "\r$(1)" >&2;
     endef
   endif
 else