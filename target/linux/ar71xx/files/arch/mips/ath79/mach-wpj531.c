Rainflowers686/RainWrt-AX3000

commit 65c25c3caf64251a992565e9ac44428d1ee9bdaa

ar71xx: wpj531: fix GPIOs for LED
/sys/devices/platform/leds-gpio/leds/ was not initialised with working LEDs.

Signed-off-by: Leon M. George <leon@georgemail.eu>

target/linux/ar71xx/files/arch/mips/ath79/mach-wpj531.c
@@ -35,10 +35,10 @@
 #include "dev-wmac.h"
 #include "machtypes.h"
 
-#define WPJ531_GPIO_LED_SIG1    14
-#define WPJ531_GPIO_LED_SIG2    15
-#define WPJ531_GPIO_LED_SIG3    22
-#define WPJ531_GPIO_LED_SIG4    23
+#define WPJ531_GPIO_LED_SIG1    13
+#define WPJ531_GPIO_LED_SIG2    14
+#define WPJ531_GPIO_LED_SIG3    15
+#define WPJ531_GPIO_LED_SIG4    16
 #define WPJ531_GPIO_BUZZER      4
 
 #define WPJ531_GPIO_BTN_RESET   17

commit 2c590ae2b316ec2d11dbbc00d219083b9781f6a4

ar71xx: wpj531: fix SIG1/RSS1 LED GPIO
In commit 65c25c3caf64 ("ar71xx: wpj531: fix GPIOs for LED") wrong GPIO
13 for SIG1/RSS1 LED was commited, the correct GPIO number for this LED
is 12.

It's listed in "Hardware Guide - wpj531 7A06 (02/07/2019)" as GPIO12/RSS1
on the LED header and same GPIO 12 is used in the vendor's SDK as well.

Fixes: 65c25c3caf64 ("ar71xx: wpj531: fix GPIOs for LED")
Signed-off-by: Leon M. George <leon@georgemail.eu>
[commit subject/message facelift]
Signed-off-by: Petr Štetiar <ynezz@true.cz>

target/linux/ar71xx/files/arch/mips/ath79/mach-wpj531.c
@@ -35,7 +35,7 @@
 #include "dev-wmac.h"
 #include "machtypes.h"
 
-#define WPJ531_GPIO_LED_SIG1    13
+#define WPJ531_GPIO_LED_SIG1    12
 #define WPJ531_GPIO_LED_SIG2    14
 #define WPJ531_GPIO_LED_SIG3    15
 #define WPJ531_GPIO_LED_SIG4    16