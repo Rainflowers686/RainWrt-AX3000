TobleMiner/openwrt

ath79: add support for Ubiquiti SW devices
SW devices are Ubiquit ToughSwitch and EdgeSwitch series devices.
Hardware-wise they are very similar to the XM device series.

Signed-off-by: Tobias Schramm <tobleminer@gmail.com>

target/linux/ath79/image/generic-ubnt.mk
@@ -51,6 +51,18 @@ define Device/ubnt-bz
   ATH_SOC := ar7241
 endef
 
+define Device/ubnt-sw
+  $(Device/ubnt)
+  ATH_SOC := ar7242
+  DEVICE_PACKAGES += kmod-usb-ohci
+  IMAGE_SIZE := 7552k
+  UBNT_BOARD := SW
+  UBNT_CHIP := ar7240
+  UBNT_TYPE := SW
+  UBNT_VERSION := 1.4.1
+  KERNEL := kernel-bin | append-dtb | relocate-kernel | lzma | uImage lzma
+endef
+
 define Device/ubnt-wa
   $(Device/ubnt)
   IMAGE_SIZE := 15744k

Rainflowers686/RainWrt-AX3000

target/linux/ath79/image/generic-ubnt.mk
@@ -47,6 +47,18 @@ define Device/ubnt-bz
   UBNT_VERSION := 6.0.0
 endef
 
+define Device/ubnt-sw
+  $(Device/ubnt)
+  SOC := ar7242
+  DEVICE_PACKAGES += kmod-usb-ohci
+  IMAGE_SIZE := 7552k
+  UBNT_BOARD := SW
+  UBNT_CHIP := ar7240
+  UBNT_TYPE := SW
+  UBNT_VERSION := 1.4.1
+  KERNEL := kernel-bin | append-dtb | relocate-kernel | lzma | uImage lzma
+endef
+
 define Device/ubnt-wa
   $(Device/ubnt)
   SOC := ar9342