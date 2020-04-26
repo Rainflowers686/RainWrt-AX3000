Rainflowers686/RainWrt-AX3000

commit d19c6d6d987b0dc944e175d54fd9179906e94614

kernel: add module to support SFP cages
Enables kernel SFP case support.

Signed-off-by: René van Dorst <opensource@vdorst.com>

package/kernel/linux/modules/netdevices.mk
@@ -1141,3 +1141,23 @@ define KernelPackage/mlx5-core/description
 endef
 
 $(eval $(call KernelPackage,mlx5-core))
+
+
+define KernelPackage/sfp
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=SFP cage support
+  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core
+  KCONFIG:= \
+	CONFIG_SFP=m \
+	CONFIG_MDIO_I2C=m
+  FILES:= \
+	$(LINUX_DIR)/drivers/net/phy/sfp.ko \
+	$(LINUX_DIR)/drivers/net/phy/mdio-i2c.ko
+  AUTOLOAD:=$(call AutoProbe,mdio-i2c sfp)
+endef
+
+define KernelPackage/SFP/description
+ Kernel module to support SFP cages
+endef
+
+$(eval $(call KernelPackage,sfp))

target/linux/ramips/image/mt7621.mk
@@ -820,7 +820,7 @@ TARGET_DEVICES += ubnt_edgerouter-x
 define Device/ubnt_edgerouter-x-sfp
   $(Device/ubnt_edgerouter_common)
   DEVICE_MODEL := EdgeRouter X SFP
-  DEVICE_PACKAGES += kmod-i2c-algo-pca kmod-gpio-pca953x
+  DEVICE_PACKAGES += kmod-i2c-algo-pca kmod-gpio-pca953x kmod-sfp
   SUPPORTED_DEVICES += ubnt-erx-sfp ubiquiti,edgerouterx-sfp
 endef
 TARGET_DEVICES += ubnt_edgerouter-x-sfp

commit b32f59fc1b8c84767530c4e25253fb118d97fdf2

kernel: netdev: fix kmod-sfp symbols
Fixes: d19c6d6d987b ("kernel: add module to support SFP cages")

Signed-off-by: David Bauer <mail@david-bauer.net>

package/kernel/linux/modules/netdevices.mk
@@ -1164,8 +1164,8 @@ define KernelPackage/sfp
   TITLE:=SFP cage support
   DEPENDS:=+kmod-i2c-core +kmod-hwmon-core +kmod-phylink
   KCONFIG:= \
-	CONFIG_SFP=m \
-	CONFIG_MDIO_I2C=m
+	CONFIG_SFP \
+	CONFIG_MDIO_I2C
   FILES:= \
 	$(LINUX_DIR)/drivers/net/phy/sfp.ko \
 	$(LINUX_DIR)/drivers/net/phy/mdio-i2c.ko