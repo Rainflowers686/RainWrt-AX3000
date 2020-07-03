Rainflowers686/RainWrt-AX3000

commit 22c11f112aeb558eff6e12929c4e2ece03a0bf2a

build,json: store arch_packages in profiles.json
The `arch_packages` contains the supported package architecture.

Previously it was necessary to parse the `Packages` index for the line
`Architecture:`, requiring both an additional parser and file download.

Signed-off-by: Paul Spooren <mail@aparcar.org>

scripts/json_overview_image_info.py
@@ -33,19 +33,22 @@ for json_file in work_dir.glob("*.json"):
             )
 
 
-output["default_packages"] = run(
+default_packages, output["arch_packages"] = run(
     [
         "make",
         "--no-print-directory",
         "-C",
         f"target/linux/{output['target'].split('/')[0]}",
         "val.DEFAULT_PACKAGES",
+        "val.ARCH_PACKAGES",
     ],
     capture_output=True,
     check=True,
     env=environ.copy().update({"TOPDIR": Path().cwd()}),
     text=True,
-).stdout.split()
+).stdout.splitlines()
+
+output["default_packages"] = default_packages.split()
 
 if output:
     output_path.write_text(json.dumps(output, sort_keys=True, separators=(",", ":")))

commit 672513977524b759a3a04e02bae11c99b0b8e29e

build,json: fix build failure in case no data is found
Only collect arch_packages if actually generating any output.

Fixes: commit 22c11f11 ("build,json: store arch_packages in profiles.json"(
Signed-off-by: Daniel Golle <daniel@makrotopia.org>
Signed-off-by: Paul Spooren <mail@aparcar.org>

scripts/json_overview_image_info.py
@@ -32,25 +32,23 @@ for json_file in work_dir.glob("*.json"):
                 image_info["profiles"][device_id]["images"][0]
             )
 
-
-default_packages, output["arch_packages"] = run(
-    [
-        "make",
-        "--no-print-directory",
-        "-C",
-        f"target/linux/{output['target'].split('/')[0]}",
-        "val.DEFAULT_PACKAGES",
-        "val.ARCH_PACKAGES",
-    ],
-    capture_output=True,
-    check=True,
-    env=environ.copy().update({"TOPDIR": Path().cwd()}),
-    text=True,
-).stdout.splitlines()
-
-output["default_packages"] = default_packages.split()
-
 if output:
+    default_packages, output["arch_packages"] = run(
+        [
+            "make",
+            "--no-print-directory",
+            "-C",
+            f"target/linux/{output['target'].split('/')[0]}",
+            "val.DEFAULT_PACKAGES",
+            "val.ARCH_PACKAGES",
+        ],
+        capture_output=True,
+        check=True,
+        env=environ.copy().update({"TOPDIR": Path().cwd()}),
+        text=True,
+    ).stdout.splitlines()
+
+    output["default_packages"] = default_packages.split()
     output_path.write_text(json.dumps(output, sort_keys=True, separators=(",", ":")))
 else:
     print("JSON info file script could not find any JSON files for target")