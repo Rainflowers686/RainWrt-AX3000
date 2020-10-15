Rainflowers686/RainWrt-AX3000

commit 2653672a582dce1bfde34fc0f6c52a15d72c4022

sunxi: BPi-M2U kernel modules for onboard WiFi
Signed-off-by: Hal Martin <hal.martin@gmail.com>

target/linux/sunxi/image/cortexa7.mk
@@ -74,7 +74,8 @@ TARGET_DEVICES += lemaker_bananapi
 define Device/lemaker_bananapi-m2-ultra
   DEVICE_VENDOR := LeMaker
   DEVICE_MODEL := Banana Pi M2 Ultra
-  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
+	brcmfmac-firmware-43430a0-sdio wpad-basic
   SOC := sun8i-r40
 endef
 TARGET_DEVICES += lemaker_bananapi-m2-ultra

commit 7f47137a5eb92c6443a66d9859b70cbbc88812d0

sunxi: use wpad-basic-wolfssl for Sinovoip Banana Pi M2 Ultra
The idea of commit 213c50477ea0 was to use wpad-basic-wolfssl
consistently throughout the whole trunk, so use it here as well.

Fixes: 2653672a582d ("BPi-M2U kernel modules for onboard WiFi")

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/sunxi/image/cortexa7.mk
@@ -75,7 +75,7 @@ define Device/sinovoip_bananapi-m2-ultra
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Ultra
   DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
-	brcmfmac-firmware-43430a0-sdio wpad-basic
+	brcmfmac-firmware-43430a0-sdio wpad-basic-wolfssl
   SUPPORTED_DEVICES:=lemaker,bananapi-m2-ultra
   SOC := sun8i-r40
 endef