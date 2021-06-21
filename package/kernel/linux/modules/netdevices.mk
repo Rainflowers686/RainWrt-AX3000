Rainflowers686/RainWrt-AX3000

commit 2b494c5269e49227040f21599ee2c2dd220145f3

kernel: add igb kernel module
Added igb kernel module which supports a variety of Intel GigE PCIe devices

Signed-off-by: Tim Harvey <tharvey@gateworks.com>

SVN-Revision: 41637

package/kernel/linux/modules/netdevices.mk
@@ -443,6 +443,24 @@ endef
 $(eval $(call KernelPackage,e1000e))
 
 
+define KernelPackage/igb
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Intel(R) 82575/82576 PCI-Express Gigabit Ethernet support"
+  DEPENDS:=@PCI_SUPPORT +kmod-i2c-algo +kmod-i2c-algo-bit
+  KCONFIG:=CONFIG_IGB \
+    CONFIG_IGB_HWMON=n \
+    CONFIG_IGB_DCA=n
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/igb/igb.ko
+  AUTOLOAD:=$(call AutoLoad,35,igb)
+endef
+
+define KernelPackage/igb/description
+ Kernel modules for Intel(R) 82575/82576 PCI-Express Gigabit Ethernet adapters.
+endef
+
+$(eval $(call KernelPackage,igb))
+
+
 define KernelPackage/b44
   TITLE:=Broadcom 44xx driver
   KCONFIG:=CONFIG_B44

commit e3c51e1e0645d386adef0d4c5a68c83d67618d67

kernel/modules: make sure igb loads at boot
Without loading the igb at boot, the recovery wouldn't have
network available. All network drivers should be loaded before
etc/board.d/02_network is called. Note that other network drivers
already have this set, such as tg3.

Fixes: 2b494c5269e4 ("kernel: add igb kernel module")
Signed-off-by: Chris Blake <chrisrblake93@gmail.com>

package/kernel/linux/modules/netdevices.mk
@@ -657,7 +657,7 @@ define KernelPackage/igb
     CONFIG_IGB_HWMON=y \
     CONFIG_IGB_DCA=n
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/igb/igb.ko
-  AUTOLOAD:=$(call AutoLoad,35,igb)
+  AUTOLOAD:=$(call AutoLoad,35,igb,1)
 endef
 
 define KernelPackage/igb/description