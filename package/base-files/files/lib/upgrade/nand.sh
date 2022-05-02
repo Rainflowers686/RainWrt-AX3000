Rainflowers686/RainWrt-AX3000

commit 072059b677ba18603e3701a569fd636db8284a31

base-files: safer sysupgrade.tar for kernel-out-of-UBI
Ensure that the kernel CRC is invalidated while rootfs is being updated.
This allows the bootloader to detect an interrupted sysupgrade and fall
back to an alternate booting method, such as TFTP, instead of just going
ahead with normal boot and effectively bricking the device.

Signed-off-by: Rodrigo Balerdi <lanchon@gmail.com>

package/base-files/files/lib/upgrade/nand.sh
@@ -303,7 +303,7 @@ nand_upgrade_tar() {
 	local has_env=0
 
 	[ "$kernel_length" != 0 -a -n "$kernel_mtd" ] && {
-		tar xf "$tar_file" ${board_dir}/kernel -O | mtd write - $CI_KERNPART
+		mtd erase $CI_KERNPART
 	}
 	[ "$kernel_length" = 0 -o ! -z "$kernel_mtd" ] && has_kernel=
 	[ "$CI_KERNPART" = "none" ] && has_kernel=
@@ -317,6 +317,10 @@ nand_upgrade_tar() {
 			ubiupdatevol /dev/$root_ubivol -s $rootfs_length -
 	}
 
+	[ "$kernel_length" != 0 -a -n "$kernel_mtd" ] && {
+		tar xf "$tar_file" ${board_dir}/kernel -O | \
+			mtd -n write - $CI_KERNPART
+	}
 	[ "$has_kernel" = "1" ] && {
 		local kern_ubivol="$( nand_find_volume $ubidev $CI_KERNPART )"
 		tar xf "$tar_file" ${board_dir}/kernel -O | \

commit 139fa0d8c48a57f2e05ba5cde4db462ff555ed70

base-files: fix sysupgrade for kernel-out-of-UBI
Commit 072059b677ba bricks devices on which the raw kernel and UBI mtd
partitions overlap.

This is the case of the ZyXEL NR7101 for example. Its OEM bootloader has
no UBI support. OpenWrt splits the stock kernel mtd partition into a raw
kernel part used by the bootloader and a UBI part used to store rootfs
and rootfs_data. Running mtd erase on the complete partition during
sysupgrade erases the UBI part and results in a soft brick.

Arguably the best solution would be to fix the partition layouts so that
kernel and UBI partitions do not overlap, also including a stock_kernel
partition to help reverting to stock firmware. This would have the added
benefit of protecting UBI from kernel images that are excessively large.

Fixes: 072059b677ba ("base-files: safer sysupgrade.tar for kernel-out-of-UBI")
Reported-by: Bjørn Mork <bjorn@mork.no>
Signed-off-by: Rodrigo Balerdi <lanchon@gmail.com>

package/base-files/files/lib/upgrade/nand.sh
@@ -305,7 +305,11 @@ nand_upgrade_tar() {
 	local ubi_kernel_length
 	if [ "$kernel_length" ]; then
 		if [ "$kernel_mtd" ]; then
-			mtd erase "$CI_KERNPART"
+			# On some devices, the raw kernel and ubi partitions overlap.
+			# These devices brick if the kernel partition is erased.
+			# Hence only invalidate kernel for now.
+			dd if=/dev/zero bs=4096 count=1 2>/dev/null | \
+				mtd write - "$CI_KERNPART"
 		else
 			ubi_kernel_length="$kernel_length"
 		fi
@@ -322,7 +326,7 @@ nand_upgrade_tar() {
 	if [ "$kernel_length" ]; then
 		if [ "$kernel_mtd" ]; then
 			tar xf "$tar_file" "$board_dir/kernel" -O | \
-				mtd -n write - "$CI_KERNPART"
+				mtd write - "$CI_KERNPART"
 		else
 			local kern_ubivol="$( nand_find_volume $ubidev "$CI_KERNPART" )"
 			tar xf "$tar_file" "$board_dir/kernel" -O | \