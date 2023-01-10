Rainflowers686/RainWrt-AX3000

commit da9d713507a06fd034293a83eb00afa286af7f54

prereq-build: add extra check for elfutils required header
While testing tools build on an alpine image it was found that with musl
libc some header are missing for elfutils tool.

Add extra prereq-build check to make sure these header are present in
the system to correctly compile host tools.

Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/prereq-build.mk
@@ -204,6 +204,18 @@ $(eval $(call SetupHostCommand,which,Please install 'which', \
 	/bin/which which, \
 	which which))
 
+$(eval $(call RequireCHeader,argp.h, \
+	Missing argp.h Please install the argp-standalone package if musl libc))
+
+$(eval $(call RequireCHeader,fts.h, \
+	Missing fts.h Please install the musl-fts-dev package if musl libc))
+
+$(eval $(call RequireCHeader,obstack.h, \
+	Missing obstack.h Please install the musl-obstack-dev package if musl libc))
+
+$(eval $(call RequireCHeader,libintl.h, \
+	Missing libintl.h Please install the musl-libintl package if musl libc))
+
 $(STAGING_DIR_HOST)/bin/mkhash: $(SCRIPT_DIR)/mkhash.c
 	mkdir -p $(dir $@)
 	$(CC) -O2 -I$(TOPDIR)/tools/include -o $@ $<

commit a136f2343cb72e7606199d693482b773618b19c3

prereq-build: limit argp/fts/obstack/libintl.h to Linux OS
BSD based OS have different fixup and doesn't require these header.
Limit these Header to Linux based OS.

Fixes: da9d713507a0 ("prereq-build: add extra check for elfutils required header")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/prereq-build.mk
@@ -204,17 +204,19 @@ $(eval $(call SetupHostCommand,which,Please install 'which', \
 	/bin/which which, \
 	which which))
 
-$(eval $(call RequireCHeader,argp.h, \
+ifeq ($(HOST_OS),Linux)
+  $(eval $(call RequireCHeader,argp.h, \
 	Missing argp.h Please install the argp-standalone package if musl libc))
 
-$(eval $(call RequireCHeader,fts.h, \
+  $(eval $(call RequireCHeader,fts.h, \
 	Missing fts.h Please install the musl-fts-dev package if musl libc))
 
-$(eval $(call RequireCHeader,obstack.h, \
+  $(eval $(call RequireCHeader,obstack.h, \
 	Missing obstack.h Please install the musl-obstack-dev package if musl libc))
 
-$(eval $(call RequireCHeader,libintl.h, \
+  $(eval $(call RequireCHeader,libintl.h, \
 	Missing libintl.h Please install the musl-libintl package if musl libc))
+endif
 
 $(STAGING_DIR_HOST)/bin/mkhash: $(SCRIPT_DIR)/mkhash.c
 	mkdir -p $(dir $@)