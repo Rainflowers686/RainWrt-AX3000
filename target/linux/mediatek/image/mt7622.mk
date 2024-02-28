Rainflowers686/RainWrt-AX3000

commit 623336f875a210d764afd3cf55313d07067379b3

mediatek: mt7622: skip build for MT7622 rfb1 (UBI)
Due to increased kernel size the build currently fails if including
the MT7622 rfb1 (UBI). Skip it for now until there is a better
solution (such as replacing the bootloader and changing the flash
layout).

Fixes: 709bdbca58 ("mediatek: switch to Linux version 6.1")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/mt7622.mk
@@ -302,7 +302,7 @@ define Device/mediatek_mt7622-rfb1-ubi
                 check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7622-rfb1-ubi
+# TARGET_DEVICES += mediatek_mt7622-rfb1-ubi
 
 define Device/netgear_wax206
   $(Device/dsa-migration)

commit 7f0ab84894825d3a03f0133c7417fff03d61ec87

mediatek: re-enable mt7622-rfb1-ubi with changed partition layout
The boot loader does not have a fixed size limit for the kernel,
so we're free to change the layout. This may break sysupgrade, but a fresh
flash from initramfs works.

Fixes: 623336f875a2 ("mediatek: mt7622: skip build for MT7622 rfb1 (UBI)")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

target/linux/mediatek/dts/mt7622-rfb1-ubi.dts
@@ -44,12 +44,12 @@
 
 			partition@200000 {
 				label = "kernel";
-				reg = <0x2c0000 0x400000>;
+				reg = <0x2c0000 0x600000>;
 			};
 
 			partition@6c0000 {
 				label = "ubi";
-				reg = <0x6c0000 0x6f00000>;
+				reg = <0x8c0000 0x6f00000>;
 			};
 
 			/delete-node/ partition@2200000;

target/linux/mediatek/image/mt7622.mk
@@ -295,14 +295,14 @@ define Device/mediatek_mt7622-rfb1-ubi
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  KERNEL_SIZE := 4194304
+  KERNEL_SIZE := 6291456
   IMAGE_SIZE := 32768k
   IMAGES += factory.bin
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
                 check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-# TARGET_DEVICES += mediatek_mt7622-rfb1-ubi
+TARGET_DEVICES += mediatek_mt7622-rfb1-ubi
 
 define Device/netgear_wax206
   $(Device/dsa-migration)