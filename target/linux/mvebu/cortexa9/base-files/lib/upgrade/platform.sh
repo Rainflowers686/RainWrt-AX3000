Rainflowers686/RainWrt-AX3000

commit f0f3a4f029f082bb19a76320e62223d8dde0da2d

mvebu: fix default partition name
The firmware partition name is specifc to ipTIME NAS1dual and should not be
set globally.

Fixes: #16148
Fixes: d50868961922 ("mvebu: add support for ipTIME NAS1dual")
Signed-off-by: Boris Krasnovskiy <borkra@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/16690
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/mvebu/cortexa9/base-files/lib/upgrade/platform.sh
@@ -6,7 +6,6 @@
 RAMFS_COPY_BIN='fw_printenv fw_setenv seq strings'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
-PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
@@ -74,6 +73,10 @@ platform_do_upgrade() {
 		esac
 		default_do_upgrade "$1"
 		;;
+	iptime,nas1dual)
+		PART_NAME=firmware
+		;;
+
 	linksys,wrt1200ac|\
 	linksys,wrt1900ac-v1|\
 	linksys,wrt1900ac-v2|\

commit e4309c7c95710b132e1539f0bf7fd24b2d2fd258

mvebu: cortex-a9: add upgrade methode to nas1dual
The blamed commit adds a upgrade recipe for nas1dual to specify the
firmware partition name, but does not actually include the recipe that
will be called.

Since it previously relied on the default one, add that one.

Fixes: f0f3a4f029f0 ("mvebu: fix default partition name")
Link: https://github.com/openwrt/openwrt/pull/16704
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/mvebu/cortexa9/base-files/lib/upgrade/platform.sh
@@ -75,8 +75,8 @@ platform_do_upgrade() {
 		;;
 	iptime,nas1dual)
 		PART_NAME=firmware
+		default_do_upgrade "$1"
 		;;
-
 	linksys,wrt1200ac|\
 	linksys,wrt1900ac-v1|\
 	linksys,wrt1900ac-v2|\