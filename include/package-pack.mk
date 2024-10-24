Rainflowers686/RainWrt-AX3000

commit f2bf046c77e97040a86a98cba1733c13de4a8ebc

include/package-pack: set missing Description on .apk creation
Set missing description info on .apk creation. This was probably a TODO
that wasn't notice when the final implementation was pushed.

Fixes: bbc87d48e1f0 ("build: add APK package build capabilities")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description:" \
+	  --info "description: $$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \

commit 3790a176ca151351eb24add9e5cae158981505da

include/package-pack: remove whitespace for Description info for APK
Remove whitespace for Description info for .apk creation

Fixes: f2bf046c77e9 ("include/package-pack: set missing Description on .apk creation")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description: $$(strip $$(Package/$(1)/description))" \
+	  --info "description:$$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \