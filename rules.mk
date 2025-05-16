Rainflowers686/RainWrt-AX3000

commit 4b2d3a0708c36d604c2ca18c5f1fe72ec8ead0ba

rules.mk: set default host tools GCC optimization level to -O2
This patch can help save over 60% of package build time. -O2
optimization level is the default value before the commit
055cc3eca8a2 ("tools: add options to optimize host binaries").

Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/18812
Signed-off-by: Robert Marko <robimarko@gmail.com>

rules.mk
@@ -278,7 +278,7 @@ PKG_CONFIG:=$(STAGING_DIR_HOST)/bin/pkg-config
 
 export PKG_CONFIG
 
-HOST_FLAGS_OPT:=$(call qstrip,$(CONFIG_HOST_FLAGS_OPT))
+HOST_FLAGS_OPT:=$(if $(CONFIG_HOST_FLAGS_OPT),$(call qstrip,$(CONFIG_HOST_FLAGS_OPT)),"-O2")
 HOST_FLAGS_STRIP:=$(call qstrip,$(CONFIG_HOST_FLAGS_STRIP))
 HOST_EXTRA_CFLAGS:=$(call qstrip,$(CONFIG_HOST_EXTRA_CFLAGS))
 HOST_EXTRA_CXXFLAGS:=$(call qstrip,$(CONFIG_HOST_EXTRA_CXXFLAGS))

commit 34b99ccb6509652b333753228c58420699f64f8c

rules.mk: respect the empty CONFIG_HOST_FLAGS_OPT
Currently, the empty CONFIG_HOST_FLAGS_OPT will be overridden by
"-O2". Fix this issue by checking if its parent menuconfig symbol
CONFIG_OPTIMIZE_HOST_TOOLS was defined.

Fixes: 4b2d3a0708c3 ("rules.mk: set default host tools GCC optimization level to -O2")
Reported-by: Christian Marangi <ansuelsmth@gmail.com>
Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/18815
Signed-off-by: Robert Marko <robimarko@gmail.com>

rules.mk
@@ -278,7 +278,7 @@ PKG_CONFIG:=$(STAGING_DIR_HOST)/bin/pkg-config
 
 export PKG_CONFIG
 
-HOST_FLAGS_OPT:=$(if $(CONFIG_HOST_FLAGS_OPT),$(call qstrip,$(CONFIG_HOST_FLAGS_OPT)),"-O2")
+HOST_FLAGS_OPT:=$(if $(CONFIG_OPTIMIZE_HOST_TOOLS),$(call qstrip,$(CONFIG_HOST_FLAGS_OPT)),-O2)
 HOST_FLAGS_STRIP:=$(call qstrip,$(CONFIG_HOST_FLAGS_STRIP))
 HOST_EXTRA_CFLAGS:=$(call qstrip,$(CONFIG_HOST_EXTRA_CFLAGS))
 HOST_EXTRA_CXXFLAGS:=$(call qstrip,$(CONFIG_HOST_EXTRA_CXXFLAGS))