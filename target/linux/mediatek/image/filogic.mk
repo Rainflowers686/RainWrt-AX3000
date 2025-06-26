Rainflowers686/RainWrt-AX3000

commit cb05b602cb1391c19426ac13b1854b5042fe43cb

mediatek: rename uboot for cmcc rax3000me
It's no longer able to decide memory type by USB port, so let's make
the name bit clear.

Please note that the CH version produced in 2025 does not have USB
port but it still uses DDR3 memory.
As far as we know, CH/XW = DDR3, WS = DDR4.

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

target/linux/mediatek/image/filogic.mk
@@ -696,14 +696,14 @@ define Device/cmcc_rax3000me
   DEVICE_DTS := mt7981b-cmcc-rax3000me
   $(call Device/cmcc_rax3000m_common)
   DEVICE_DTS_OVERLAY += mt7981b-cmcc-rax3000me-nousb
-  ARTIFACTS += nand-ddr3-preloader.bin nand-ddr3-bl31-uboot.fip \
-	nand-ddr4-preloader.bin nand-ddr4-bl31-uboot.fip
+  ARTIFACTS += nand-ddr3-preloader.bin nand-usb-bl31-uboot.fip \
+	nand-nousb-preloader.bin nand-ddr4-bl31-uboot.fip
   ARTIFACT/emmc-preloader.bin := mt7981-bl2 emmc-ddr3
   ARTIFACT/emmc-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-emmc
   ARTIFACT/nand-ddr3-preloader.bin := mt7981-bl2 spim-nand-ddr3
-  ARTIFACT/nand-ddr3-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-ddr3
   ARTIFACT/nand-ddr4-preloader.bin := mt7981-bl2 spim-nand-ddr4
-  ARTIFACT/nand-ddr4-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-ddr4
+  ARTIFACT/nand-usb-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-usb
+  ARTIFACT/nand-nousb-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-nousb
 endef
 TARGET_DEVICES += cmcc_rax3000me
 

package/boot/uboot-mediatek/Makefile
@@ -291,11 +291,11 @@ define U-Boot/mt7981_cmcc_rax3000me-emmc
   DEPENDS:=+trusted-firmware-a-mt7981-emmc-ddr3
 endef
 
-define U-Boot/mt7981_cmcc_rax3000me-nand-ddr3
+define U-Boot/mt7981_cmcc_rax3000me-nand-usb
   NAME:=CMCC RAX3000Me
   BUILD_SUBTARGET:=filogic
   BUILD_DEVICES:=cmcc_rax3000me
-  UBOOT_CONFIG:=mt7981_cmcc_rax3000me-nand-ddr3
+  UBOOT_CONFIG:=mt7981_cmcc_rax3000me-nand-usb
   UBOOT_IMAGE:=u-boot.fip
   BL2_BOOTDEV:=spim-nand
   BL2_SOC:=mt7981
@@ -303,11 +303,11 @@ define U-Boot/mt7981_cmcc_rax3000me-nand-ddr3
   DEPENDS:=+trusted-firmware-a-mt7981-spim-nand-ddr3
 endef
 
-define U-Boot/mt7981_cmcc_rax3000me-nand-ddr4
+define U-Boot/mt7981_cmcc_rax3000me-nand-nousb
   NAME:=CMCC RAX3000Me
   BUILD_SUBTARGET:=filogic
   BUILD_DEVICES:=cmcc_rax3000me
-  UBOOT_CONFIG:=mt7981_cmcc_rax3000me-nand-ddr4
+  UBOOT_CONFIG:=mt7981_cmcc_rax3000me-nand-nousb
   UBOOT_IMAGE:=u-boot.fip
   BL2_BOOTDEV:=spim-nand
   BL2_SOC:=mt7981
@@ -1017,8 +1017,8 @@ UBOOT_TARGETS := \
 	mt7981_cmcc_rax3000m-emmc \
 	mt7981_cmcc_rax3000m-nand \
 	mt7981_cmcc_rax3000me-emmc \
-	mt7981_cmcc_rax3000me-nand-ddr3 \
-	mt7981_cmcc_rax3000me-nand-ddr4 \
+	mt7981_cmcc_rax3000me-nand-usb \
+	mt7981_cmcc_rax3000me-nand-nousb \
 	mt7981_cudy_tr3000-v1 \
 	mt7981_gatonetworks_gdsp \
 	mt7981_glinet_gl-x3000 \

package/boot/uboot-mediatek/patches/471-add-cmcc_rax3000me.patch
@@ -125,7 +125,7 @@
 +CONFIG_ZSTD=y
 +CONFIG_HEXDUMP=y
 --- /dev/null
-+++ b/configs/mt7981_cmcc_rax3000me-nand-ddr3_defconfig
++++ b/configs/mt7981_cmcc_rax3000me-nand-usb_defconfig
 @@ -0,0 +1,123 @@
 +CONFIG_ARM=y
 +CONFIG_SYS_HAS_NONCACHED_MEMORY=y
@@ -200,7 +200,7 @@
 +CONFIG_ENV_UBI_VOLUME_REDUND="ubootenv2"
 +CONFIG_SYS_RELOC_GD_ENV_ADDR=y
 +CONFIG_USE_DEFAULT_ENV_FILE=y
-+CONFIG_DEFAULT_ENV_FILE="defenvs/cmcc_rax3000me-nand-ddr3_env"
++CONFIG_DEFAULT_ENV_FILE="defenvs/cmcc_rax3000me-nand-usb_env"
 +CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG=y
 +CONFIG_VERSION_VARIABLE=y
 +CONFIG_NETCONSOLE=y
@@ -251,7 +251,7 @@
 +CONFIG_ZSTD=y
 +CONFIG_HEXDUMP=y
 --- /dev/null
-+++ b/configs/mt7981_cmcc_rax3000me-nand-ddr4_defconfig
++++ b/configs/mt7981_cmcc_rax3000me-nand-nousb_defconfig
 @@ -0,0 +1,123 @@
 +CONFIG_ARM=y
 +CONFIG_SYS_HAS_NONCACHED_MEMORY=y
@@ -326,7 +326,7 @@
 +CONFIG_ENV_UBI_VOLUME_REDUND="ubootenv2"
 +CONFIG_SYS_RELOC_GD_ENV_ADDR=y
 +CONFIG_USE_DEFAULT_ENV_FILE=y
-+CONFIG_DEFAULT_ENV_FILE="defenvs/cmcc_rax3000me-nand-ddr4_env"
++CONFIG_DEFAULT_ENV_FILE="defenvs/cmcc_rax3000me-nand-nousb_env"
 +CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG=y
 +CONFIG_VERSION_VARIABLE=y
 +CONFIG_NETCONSOLE=y
@@ -459,7 +459,7 @@
 +_switch_to_menu=setenv _switch_to_menu ; setenv bootdelay 3 ; setenv bootmenu_delay 3 ; setenv bootmenu_0 $bootmenu_0d ; setenv bootmenu_0d ; run _bootmenu_update_title
 +_bootmenu_update_title=setenv _bootmenu_update_title ; setenv bootmenu_title "$bootmenu_title       [33m$ver[0m"
 --- /dev/null
-+++ b/defenvs/cmcc_rax3000me-nand-ddr3_env
++++ b/defenvs/cmcc_rax3000me-nand-usb_env
 @@ -0,0 +1,56 @@
 +ipaddr=192.168.1.1
 +serverip=192.168.1.254
@@ -470,7 +470,7 @@
 +bootdelay=0
 +bootfile=immortalwrt-mediatek-filogic-cmcc_rax3000me-initramfs-recovery.itb
 +bootfile_bl2=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-ddr3-preloader.bin
-+bootfile_fip=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-ddr3-bl31-uboot.fip
++bootfile_fip=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-usb-bl31-uboot.fip
 +bootfile_upg=immortalwrt-mediatek-filogic-cmcc_rax3000me-squashfs-sysupgrade.itb
 +bootled_pwr=red:status
 +bootled_rec=blue:status
@@ -518,7 +518,7 @@
 +_switch_to_menu=setenv _switch_to_menu ; setenv bootdelay 3 ; setenv bootmenu_delay 3 ; setenv bootmenu_0 $bootmenu_0d ; setenv bootmenu_0d ; run _bootmenu_update_title
 +_bootmenu_update_title=setenv _bootmenu_update_title ; setenv bootmenu_title "$bootmenu_title       [33m$ver[0m"
 --- /dev/null
-+++ b/defenvs/cmcc_rax3000me-nand-ddr4_env
++++ b/defenvs/cmcc_rax3000me-nand-nousb_env
 @@ -0,0 +1,56 @@
 +ipaddr=192.168.1.1
 +serverip=192.168.1.254
@@ -529,7 +529,7 @@
 +bootdelay=0
 +bootfile=immortalwrt-mediatek-filogic-cmcc_rax3000me-initramfs-recovery.itb
 +bootfile_bl2=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-ddr4-preloader.bin
-+bootfile_fip=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-ddr4-bl31-uboot.fip
++bootfile_fip=immortalwrt-mediatek-filogic-cmcc_rax3000me-nand-nousb-bl31-uboot.fip
 +bootfile_upg=immortalwrt-mediatek-filogic-cmcc_rax3000me-squashfs-sysupgrade.itb
 +bootled_pwr=red:status
 +bootled_rec=blue:status

commit 84814b0ed8beabc9418b36b70091d2357fcb9ecc

mediatek: fix typo error
Fixes: cb05b602cb13 ("mediatek: rename uboot for cmcc rax3000me")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

target/linux/mediatek/image/filogic.mk
@@ -696,8 +696,8 @@ define Device/cmcc_rax3000me
   DEVICE_DTS := mt7981b-cmcc-rax3000me
   $(call Device/cmcc_rax3000m_common)
   DEVICE_DTS_OVERLAY += mt7981b-cmcc-rax3000me-nousb
-  ARTIFACTS += nand-ddr3-preloader.bin nand-usb-bl31-uboot.fip \
-	nand-nousb-preloader.bin nand-ddr4-bl31-uboot.fip
+  ARTIFACTS += nand-ddr3-preloader.bin nand-ddr4-preloader.bin \
+	nand-usb-bl31-uboot.fip nand-nousb-bl31-uboot.fip
   ARTIFACT/emmc-preloader.bin := mt7981-bl2 emmc-ddr3
   ARTIFACT/emmc-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-emmc
   ARTIFACT/nand-ddr3-preloader.bin := mt7981-bl2 spim-nand-ddr3