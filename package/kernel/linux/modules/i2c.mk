Rainflowers686/RainWrt-AX3000

commit 88f6f29a73d1044b95c1c10ae0e65d2db22e85f6

kernel: package modules for I2C HID devices
Package driver modules for I2C HID devices such as touchpads,
touchscreens and trackpoints found on some laptops. Only the ACPI
firmware variant of the driver is packaged for now as that's what I got
for testing.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/linux/modules/i2c.mk
@@ -162,6 +162,38 @@ endef
 $(eval $(call KernelPackage,i2c-gpio))
 
 
+I2C_HID_MODULES:= \
+  CONFIG_I2C_HID_CORE:drivers/hid/i2c-hid/i2c-hid
+
+define KernelPackage/i2c-hid
+  $(call i2c_defaults,$(I2C_HID_MODULES),60)
+  TITLE:=I2C HID support
+  KCONFIG+= CONFIG_I2C_HID
+  DEPENDS:=+kmod-hid
+  HIDDEN:=1
+endef
+
+$(eval $(call KernelPackage,i2c-hid))
+
+
+I2C_HID_ACPI_MODULES:= \
+  CONFIG_I2C_HID_ACPI:drivers/hid/i2c-hid/i2c-hid-acpi
+
+define KernelPackage/i2c-hid-acpi
+  $(call i2c_defaults,$(I2C_HID_ACPI_MODULES),61)
+  TITLE:=HID over I2C transport layer ACPI driver
+  DEPENDS:=@TARGET_armsr_armv8||TARGET_loongarch64||TARGET_x86 +kmod-i2c-hid
+endef
+
+define KernelPackage/i2c-hid-acpi/description
+  Support for keyboard, touchpad, touchscreen, or any
+  other HID based devices which is connected to your computer via I2C.
+  This driver supports ACPI-based systems.
+endef
+
+$(eval $(call KernelPackage,i2c-hid-acpi))
+
+
 I2C_I801_MODULES:= \
   CONFIG_I2C_I801:drivers/i2c/busses/i2c-i801
 

commit af395b548a8060022651ff0626c7dd2f29739c65

kernel: add missing dependency on kmod-drm to kmod-i2c-hid
Buildbot revealed that on the x86/geode target, on which CONFIG_DRM=y isn't set,
kmod-i2c-hid misses the dependency on kmod-drm. Fix that to fix the build for
x86/geode.

Fixes: 88f6f29a73 ("kernel: package modules for I2C HID devices")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/linux/modules/i2c.mk
@@ -169,7 +169,7 @@ define KernelPackage/i2c-hid
   $(call i2c_defaults,$(I2C_HID_MODULES),60)
   TITLE:=I2C HID support
   KCONFIG+= CONFIG_I2C_HID
-  DEPENDS:=+kmod-hid
+  DEPENDS:=+kmod-drm +kmod-hid
   HIDDEN:=1
 endef
 