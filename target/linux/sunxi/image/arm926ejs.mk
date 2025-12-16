Rainflowers686/RainWrt-AX3000

commit aa29494008115b8bcb79aac464900ca8cbcb329a

sunxi: add support for Banana Pi M2 Berry
CPU: Allwinner V40 quad-core Cortex A7 @ 1.2GHz
Memory: 1GB DDR3
Storage: SDcard, native SATA
Network: 10/100/1000M ethernet, Ampak AP6212 wifi + BT
USB: 4x USB 2.0

Installation:
Use the standard sunxi installation to an SD-card.

While the board is very similar to the M2 Ultra board
(the V40 is the automotive version of the R40), as both
the u-boot and kernel supports them separately, and some
pins are different, let's add a separate device spec.

Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>

package/boot/uboot-sunxi/Makefile
@@ -319,6 +319,12 @@ define U-Boot/Bananapi_M2_Ultra
   BUILD_DEVICES:=sinovoip_bananapi-m2-ultra
 endef
 
+define U-Boot/bananapi_m2_berry
+  BUILD_SUBTARGET:=cortexa7
+  NAME:=Bananapi M2 Berry
+  BUILD_DEVICES:=sinovoip_bananapi-m2-berry
+endef
+
 UBOOT_TARGETS := \
 	a64-olinuxino \
 	a64-olinuxino-emmc \
@@ -330,6 +336,7 @@ UBOOT_TARGETS := \
 	A20-OLinuXino_MICRO \
 	bananapi_m2_plus_h3 \
 	Bananapi \
+	bananapi_m2_berry \
 	Bananapi_M2_Ultra \
 	Bananapro \
 	Cubieboard \

target/linux/sunxi/image/cortexa7.mk
@@ -77,6 +77,16 @@ define Device/lemaker_bananapi
 endef
 TARGET_DEVICES += lemaker_bananapi
 
+define Device/sinovoip_bananapi-m2-berry
+  DEVICE_VENDOR := Sinovoip
+  DEVICE_MODEL := Banana Pi M2 Berry
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
+	brcmfmac-firmware-43430-sdio wpad-basic-wolfssl
+  SUPPORTED_DEVICES:=lemaker,bananapi-m2-berry
+  SOC := sun8i-v40
+endef
+TARGET_DEVICES += sinovoip_bananapi-m2-berry
+
 define Device/sinovoip_bananapi-m2-ultra
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Ultra

commit 666f222e698a1c1f971bd524c0e5634a53325b76

sunxi: image: sync target profiles names with DT compatible
Following up with errors reported in the ASU repo, these bananapi cases
do not match the DT compatible "bpi", sync with dts sources.
Also some profiles were overwriting SUPPORTED_DEVICES.
Sysupgrade would be failing in SUPPORTED_DEVICES check since
the DT compatible(/tmp/sysinfo/board_name) is not in SUPPORTED_DEVICES.
This should also fix errors when using ASU sysupgrade clients.
- Sync profile makefile target names with DT compatibles.
- Fix overwrites of SUPPORTED_DEVICES instead of appending.
- Adapt the uboot-sunxi profiles accordingly.
*bpi-p2-zero dts is still not upstream.

V2:
- Include fixes for arm926ejs(ARM926EJ-S) subtarget (LicheePi Nano and
PopStick v1.1) (profile rename for correct default SUPPORTED_DEVICES)

Fixes: https://forum.openwrt.org/t/luci-attended-sysupgrade-support-thread/230552/246
Fixes: https://github.com/openwrt/asu/issues/486
Fixes: https://github.com/openwrt/asu/issues/524
Fixes: aa29494008115b8bcb79aac464900ca8cbcb329a "sunxi: add support for Banana Pi M2 Berry"
Fixes: af83d0f98e5fede24809215410262e21d04f53e5 "sunxi: add support for Sinovoip Banana Pi M2 Plus"
Fixes: 1a29ae652a010fcbd12bddecabcac5a0d6d26edd "sunxi: Add support for Banana Pi M2 Ultra"
Fixes: a1b3623adfbd0f43a57266b5b974ac3b1cad02b9 "sunxi: add support for Bananapi P2 Zero"
Fixes: 68ae537e12cfb5ef42a13d874ebc1486a5c63689 "sunxi: add support for Banana Pi M3"
Fixes: 3a9f45351f8ed76c9278f6461c7fae5095ffe094 "sunxi: add support for NanoPi NEO Plus2 board"
Fixes: bd822b79123a031d71bb3899a1d46e59367acf6a "sunxi: build image/uboot for the NanoPi NEO2"
Fixes: 7ba3a9b77f76de926b951480745d6402ea4462bf "sunxi: add support for FriendlyARM NanoPi R1S H5"
Fixes: c3987ecfafd5229105ff0d30aef3d9b9436cd17d "sunxi: add F1C100 (arm926ej-s) support"

Signed-off-by: Mario Andrés Pérez <mapb_@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/21095
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/boot/uboot-sunxi/Makefile
@@ -134,17 +134,17 @@ define U-Boot/Mele_M9
   BUILD_DEVICES:=mele_m9
 endef
 
-define U-Boot/OLIMEX_A13_SOM
+define U-Boot/a13-som
   BUILD_SUBTARGET:=cortexa8
   NAME:=Olimex A13 SOM
-  BUILD_DEVICES:=olimex_a13-olimex-som
+  BUILD_DEVICES:=olimex_a13-olinuxino
 endef
 
 define U-Boot/licheepi_nano
   BUILD_SUBTARGET:=arm926ejs
   NAME:=LicheePi Nano
   UENV:=f1c
-  BUILD_DEVICES:=licheepi-nano
+  BUILD_DEVICES:=licheepi_licheepi-nano
 endef
 
 define U-Boot/Linksprite_pcDuino
@@ -268,7 +268,7 @@ define U-Boot/popstick
   BUILD_SUBTARGET:=arm926ejs
   NAME:=PopStick
   UENV:=f1c
-  BUILD_DEVICES:=popstick-v1.1
+  BUILD_DEVICES:=sourceparts_popstick-v1.1
 endef
 
 define U-Boot/libretech_all_h3_cc_h5
@@ -316,16 +316,16 @@ define U-Boot/pine64_plus
   ATF:=a64
 endef
 
-define U-Boot/bananapi_m2_plus_h3
+define U-Boot/bpi_m2_plus_h3
   BUILD_SUBTARGET:=cortexa7
   NAME:=Bananapi M2 Plus H3
-  BUILD_DEVICES:=sinovoip_bananapi-m2-plus
+  BUILD_DEVICES:=sinovoip_bpi-m2-plus
 endef
 
-define U-Boot/Sinovoip_BPI_M3
+define U-Boot/bpi_m3
   BUILD_SUBTARGET:=cortexa7
   NAME:=Bananapi M3
-  BUILD_DEVICES:=sinovoip_bananapi-m3
+  BUILD_DEVICES:=sinovoip_bpi-m3
 endef
 
 define U-Boot/sopine_baseboard
@@ -382,22 +382,22 @@ define U-Boot/orangepi_zero3
   ATF:=h616
 endef
 
-define U-Boot/Bananapi_M2_Ultra
+define U-Boot/bpi_m2_ultra
   BUILD_SUBTARGET:=cortexa7
   NAME:=Bananapi M2 Ultra
-  BUILD_DEVICES:=sinovoip_bananapi-m2-ultra
+  BUILD_DEVICES:=sinovoip_bpi-m2-ultra
 endef
 
-define U-Boot/bananapi_m2_berry
+define U-Boot/bpi_m2_berry
   BUILD_SUBTARGET:=cortexa7
   NAME:=Bananapi M2 Berry
-  BUILD_DEVICES:=sinovoip_bananapi-m2-berry
+  BUILD_DEVICES:=sinovoip_bpi-m2-berry
 endef
 
-define U-Boot/bananapi_p2_zero
+define U-Boot/bpi_p2_zero
   BUILD_SUBTARGET:=cortexa7
   NAME:=Bananapi P2 Zero
-  BUILD_DEVICES:=sinovoip_bananapi-p2-zero
+  BUILD_DEVICES:=sinovoip_bpi-p2-zero
 endef
 
 
@@ -406,24 +406,25 @@ UBOOT_TARGETS := \
 	a64-olinuxino-emmc \
 	A10-OLinuXino-Lime \
 	A13-OLinuXino \
+	a13-som \
 	A20-OLinuXino-Lime \
 	A20-OLinuXino-Lime2 \
 	A20-OLinuXino-Lime2-eMMC \
 	A20-OLinuXino_MICRO \
-	bananapi_m2_plus_h3 \
-	Bananapi \
-	bananapi_m2_berry \
-	bananapi_p2_zero \
-	Bananapi_M2_Ultra \
-	Bananapro \
 	beelink_x2 \
+	bpi_m2_berry \
+	bpi_m2_plus_h3 \
+	bpi_m2_ultra \
+	bpi_m3 \
+	bpi_p2_zero \
+	Bananapi \
+	Bananapro \
 	Cubieboard \
 	Cubieboard2 \
 	Cubietruck \
 	Hummingbird_A31 \
 	Marsboard_A10 \
 	Mele_M9 \
-	OLIMEX_A13_SOM \
 	licheepi_nano \
 	LicheePi_Zero \
 	Linksprite_pcDuino \
@@ -453,7 +454,6 @@ UBOOT_TARGETS := \
 	pangolin \
 	popstick \
 	pine64_plus \
-	Sinovoip_BPI_M3 \
 	sopine_baseboard \
 	orangepi_zero_plus \
 	libretech_all_h3_cc_h5

target/linux/sunxi/image/arm926ejs.mk
@@ -7,20 +7,21 @@
 
 KERNEL_LOADADDR=0x81000000
 
-define Device/licheepi-nano
+define Device/licheepi_licheepi-nano
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := LicheePi
-  DEVICE_MODEL := Nano
+  DEVICE_MODEL := LicheePi Nano
   DEVICE_PACKAGES := kmod-rtc-sunxi
   SOC := suniv-f1c100s
 endef
-TARGET_DEVICES += licheepi-nano
+TARGET_DEVICES += licheepi_licheepi-nano
 
-define Device/popstick-v1.1
+define Device/sourceparts_popstick-v1.1
   $(call Device/FitImageGzip)
-  DEVICE_VENDOR := PopStick
-  DEVICE_MODEL := v1.1
+  DEVICE_VENDOR := SourceParts
+  DEVICE_MODEL := PopStick
+  DEVICE_VARIANT := v1.1
   DEVICE_PACKAGES := kmod-rtc-sunxi
   SOC := suniv-f1c200s
 endef
-TARGET_DEVICES += popstick-v1.1
+TARGET_DEVICES += sourceparts_popstick-v1.1

target/linux/sunxi/image/cortexa53.mk
@@ -39,7 +39,7 @@ endef
 define Device/friendlyarm_nanopi-neo-plus2
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi NEO Plus2
-  SUPPORTED_DEVICES:=nanopi-neo-plus2
+  SUPPORTED_DEVICES += nanopi-neo-plus2
   $(Device/sun50i-h5)
 endef
 TARGET_DEVICES += friendlyarm_nanopi-neo-plus2
@@ -47,7 +47,7 @@ TARGET_DEVICES += friendlyarm_nanopi-neo-plus2
 define Device/friendlyarm_nanopi-neo2
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi NEO2
-  SUPPORTED_DEVICES:=nanopi-neo2
+  SUPPORTED_DEVICES += nanopi-neo2
   $(Device/sun50i-h5)
 endef
 TARGET_DEVICES += friendlyarm_nanopi-neo2
@@ -56,7 +56,7 @@ define Device/friendlyarm_nanopi-r1s-h5
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := Nanopi R1S H5
   DEVICE_PACKAGES := kmod-gpio-button-hotplug kmod-usb-net-rtl8152
-  SUPPORTED_DEVICES:=nanopi-r1s-h5
+  SUPPORTED_DEVICES += nanopi-r1s-h5
   $(Device/sun50i-h5)
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r1s-h5
@@ -109,7 +109,6 @@ define Device/xunlong_orangepi-one-plus
   $(Device/sun50i-h6)
   DEVICE_VENDOR := Xunlong
   DEVICE_MODEL := Orange Pi One Plus
-  SUNXI_DTS_DIR := allwinner/
 endef
 TARGET_DEVICES += xunlong_orangepi-one-plus
 

target/linux/sunxi/image/cortexa7.mk
@@ -92,27 +92,29 @@ define Device/lemaker_bananapi
 endef
 TARGET_DEVICES += lemaker_bananapi
 
-define Device/sinovoip_bananapi-m2-berry
+define Device/sinovoip_bpi-m2-berry
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Berry
   DEVICE_PACKAGES:=kmod-ata-sunxi kmod-brcmfmac \
 	cypress-firmware-43430-sdio wpad-basic-mbedtls
-  SUPPORTED_DEVICES:=lemaker,bananapi-m2-berry
+  SUPPORTED_DEVICES += lemaker,bananapi-m2-berry
   SOC := sun8i-v40
+  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-bananapi-m2-berry
 endef
-TARGET_DEVICES += sinovoip_bananapi-m2-berry
+TARGET_DEVICES += sinovoip_bpi-m2-berry
 
-define Device/sinovoip_bananapi-m2-ultra
+define Device/sinovoip_bpi-m2-ultra
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Ultra
   DEVICE_PACKAGES:=kmod-ata-sunxi kmod-brcmfmac \
 	brcmfmac-firmware-43430a0-sdio wpad-basic-mbedtls
-  SUPPORTED_DEVICES:=lemaker,bananapi-m2-ultra
+  SUPPORTED_DEVICES += lemaker,bananapi-m2-ultra
   SOC := sun8i-r40
+  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-bananapi-m2-ultra
 endef
-TARGET_DEVICES += sinovoip_bananapi-m2-ultra
+TARGET_DEVICES += sinovoip_bpi-m2-ultra
 
 define Device/lemaker_bananapro
   $(call Device/FitImageGzip)
@@ -217,35 +219,38 @@ define Device/roofull_beelink-x2
 endef
 TARGET_DEVICES += roofull_beelink-x2
 
-define Device/sinovoip_bananapi-m2-plus
+define Device/sinovoip_bpi-m2-plus
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2+
   DEVICE_PACKAGES:=kmod-leds-gpio kmod-brcmfmac \
 	brcmfmac-firmware-43430a0-sdio wpad-basic-mbedtls
   SOC := sun8i-h3
+  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-bananapi-m2-plus
 endef
-TARGET_DEVICES += sinovoip_bananapi-m2-plus
+TARGET_DEVICES += sinovoip_bpi-m2-plus
 
-define Device/sinovoip_bananapi-m3
+define Device/sinovoip_bpi-m3
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M3
   DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-leds-gpio kmod-rtc-ac100 \
 	kmod-brcmfmac cypress-firmware-43430-sdio wpad-basic-mbedtls
   SOC := sun8i-a83t
+  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-bananapi-m3
 endef
-TARGET_DEVICES += sinovoip_bananapi-m3
+TARGET_DEVICES += sinovoip_bpi-m3
 
-define Device/sinovoip_bananapi-p2-zero
+define Device/sinovoip_bpi-p2-zero
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi P2 Zero
   DEVICE_PACKAGES:=kmod-leds-gpio kmod-brcmfmac \
 	cypress-firmware-43430-sdio wpad-basic-mbedtls
   SOC := sun8i-h2-plus
+  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-bananapi-p2-zero
 endef
-TARGET_DEVICES += sinovoip_bananapi-p2-zero
+TARGET_DEVICES += sinovoip_bpi-p2-zero
 
 define Device/xunlong_orangepi-one
   $(call Device/FitImageGzip)

target/linux/sunxi/image/cortexa8.mk
@@ -43,21 +43,12 @@ define Device/olimex_a10-olinuxino-lime
 endef
 TARGET_DEVICES += olimex_a10-olinuxino-lime
 
-define Device/olimex_a13-olimex-som
-  $(call Device/FitImageGzip)
-  DEVICE_VENDOR := Olimex
-  DEVICE_MODEL := A13-SOM
-  DEVICE_PACKAGES:=kmod-rtl8192cu
-  SUPPORTED_DEVICES:=olimex,a13-olinuxino
-  SOC := sun5i-a13
-  SUNXI_DTS := $$(SUNXI_DTS_DIR)sun5i-a13-olinuxino
-endef
-TARGET_DEVICES += olimex_a13-olimex-som
-
 define Device/olimex_a13-olinuxino
   $(call Device/FitImageGzip)
   DEVICE_VENDOR := Olimex
   DEVICE_MODEL := A13-OLinuXino
+  DEVICE_ALT0_VENDOR := Olimex
+  DEVICE_ALT0_MODEL := A13-SOM
   DEVICE_PACKAGES:=kmod-rtl8192cu
   SOC := sun5i
 endef